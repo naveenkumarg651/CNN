faa855e4820e5b411da69ea3bdccf22bb527a7dc
Naveen Kumar <32532060+naveenkumarg651@users.noreply.github.com>
2018-12-14 20:59:43 +0530

    Create newone.py

---
+++ project_1/newone.py
@@ -0,0 +1,123 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "runfile('G:/mypython/project_1/paddy.py', wdir='G:/mypython/project_1')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "?"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%quickref"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%python\n",
+    "import numpy\n",
+    "print(numpy.mat([1,2]))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%python"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%html\n",
+    "<html>\n",
+    "<head></head>\n",
+    "<body><p1>hello</p1></body>\n",
+    "</html>"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%html\n",
+    "<html>\n",
+    "<head>\n",
+    "<script type=\"text.py\">\n",
+    "print(\"hello\")\n",
+    "</script>\n",
+    "\n",
+    "</head>\n",
+    "<body><p1>hello</p1></body>\n",
+    "</html>"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%html\n",
+    "<html>\n",
+    "<head>\n",
+    "<script type=\"text.py\">\n",
+    "print(\"hello\")\n",
+    "for i in range(10):\n",
+    "    print(i)\n",
+    "</script>\n",
+    "\n",
+    "</head>\n",
+    "<body><p1>hello</p1></body>\n",
+    "</html>"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%quickref"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%notebook"
+   ]
+  }
+ ],
+ "metadata": {},
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
